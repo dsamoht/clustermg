3a54de215b3fc2d25793656733005e9203ac0852
Nathan Lupien <nathan.lupien.1@ulaval.ca>
2024-06-12 13:31:37 -0400

    Corrected output of hmmer_summary

--- bin/hmmer_summary.py
+++ bin/hmmer_summary.py
@@ -5,7 +5,7 @@ usage:
 Write a contig annotation table (contig_annotation.tsv) with results from Pfam and/or Kegg Ortholog.
 """
 import pandas as pd
-import os.path
+import os
 import argparse
 
 ap = argparse.ArgumentParser()
@@ -26,12 +26,11 @@ namesList = ["contigId", "accession_target", "tlen",
 
 
 # Pfam
+df_pfam = pd.DataFrame(columns=["contigId", "genPred_len", "pfam_name", "pfam_id", "pfam_qlen", "pfam_E_value"], dtype=float)
 if args['pfamPath'] != None:
     with open(args['pfamPath'], 'r') as fp:
         lines = len(fp.readlines())
-    if lines <= 13:
-        df_pfam = pd.DataFrame(columns=["contigId", "genPred_len", "pfam_name", "pfam_id", "pfam_qlen", "pfam_E_value"])
-    else:
+    if lines > 13:
         df_pfam = pd.read_csv(args['pfamPath'], sep=" ", skiprows=3, skipfooter=10, header=None,
                         skipinitialspace=True, usecols=list(range(0, 22)),
                         names=namesList, na_values="-", engine="python")
@@ -39,25 +38,22 @@ if args['pfamPath'] != None:
         df_pfam = df_pfam.drop_duplicates(subset=["contigId"], keep="first")
         df_pfam = df_pfam.iloc[:,[0,2,3,4,5,6]]
         df_pfam.columns = ["contigId", "genPred_len", "pfam_name", "pfam_id", "pfam_qlen", "pfam_E_value"]
-else:
-    df_pfam = pd.DataFrame(columns=["contigId", "genPred_len", "pfam_name", "pfam_id", "pfam_qlen", "pfam_E_value"])
 
 
 # Kegg
+df_kegg = pd.DataFrame(columns=["contigId", "genPred_len", "kegg_name", "kegg_id", "kegg_qlen", "kegg_E_value"], dtype=float)
 if args['keggPath'] != None:
-    def swap_columns(df, col1, col2):
-        col_list = list(df.columns)
-        x, y = col_list.index(col1), col_list.index(col2)
-        col_list[y], col_list[x] = col_list[x], col_list[y]
-        df = df[col_list]
-        return df
-
-    ko_list = pd.read_csv(args['koList'], sep="\t")
     with open(args['keggPath'], 'r') as fp:
         lines = len(fp.readlines())
-    if lines <= 13:
-        df_kegg = pd.DataFrame(columns=["contigId", "genPred_len", "pfam_name", "pfam_id", "pfam_qlen", "pfam_E_value"])
-    else:
+    if lines > 13:
+        def swap_columns(df, col1, col2):
+            col_list = list(df.columns)
+            x, y = col_list.index(col1), col_list.index(col2)
+            col_list[y], col_list[x] = col_list[x], col_list[y]
+            df = df[col_list]
+            return df
+
+        ko_list = pd.read_csv(args['koList'], sep="\t")
         df_kegg = pd.read_csv(args['keggPath'], sep=" ", skiprows=3, skipfooter=10, header=None,
                         skipinitialspace=True, usecols=list(range(0, 22)),
                         na_values="-", engine="python")
@@ -70,24 +66,24 @@ if args['keggPath'] != None:
         df_kegg = df_kegg.drop_duplicates(subset=["contigId"], keep="first")
         df_kegg = df_kegg.iloc[:,[0,2,3,4,5,6]]
         df_kegg.columns = ["contigId", "genPred_len", "kegg_name", "kegg_id", "kegg_qlen", "kegg_E_value"]
-else:
-    df_kegg = pd.DataFrame(columns=["contigId", "genPred_len", "kegg_name", "kegg_id", "kegg_qlen", "kegg_E_value"])
+        
 
 
 # Diammond
+df_diammond = pd.DataFrame(columns=["contigId", "diam_name", "diam_seq_identity", "diam_aln_len", "diam_E_value"], dtype=float)
 if args['diamPath'] != None:
-    df_diammond = pd.read_csv(args['diamPath'], sep="\t",
-                            usecols=[0, 1, 2, 3, 10], header=None)
-    df_diammond.columns = ["contigId", "diam_name", "diam_seq_identity", "diam_aln_len", "diam_E_value"]
-    df_diammond = df_diammond.sort_values(by="diam_E_value")
-    df_diammond = df_diammond.drop_duplicates(subset=["contigId"], keep="first")
-else:
-    df_diammond = pd.DataFrame(columns=["contigId", "diam_name", "diam_seq_identity", "diam_aln_len", "diam_E_value"])
+    if os.stat(args['diamPath']).st_size != 0:
+        df_diammond = pd.read_csv(args['diamPath'], sep="\t",
+                                usecols=[0, 1, 2, 3, 10], header=None)
+        df_diammond.columns = ["contigId", "diam_name", "diam_seq_identity", "diam_aln_len", "diam_E_value"]
+        df_diammond = df_diammond.sort_values(by="diam_E_value")
+        df_diammond = df_diammond.drop_duplicates(subset=["contigId"], keep="first")
 
 
 # Merge
 df_annot = pd.merge(left=df_pfam, right=df_kegg, left_on="contigId", right_on="contigId", how="outer")
 df_annot = pd.merge(left=df_annot, right=df_diammond, left_on="contigId", right_on="contigId", how="outer")
+df_annot.insert(0, 'contigId', df_annot.pop('contigId'))
 df_annot.loc[(df_annot['genPred_len_x'].isna()) & (df_annot['genPred_len_y'].notna()), 'genPred_len_x'] = df_annot['genPred_len_y'][(df_annot['genPred_len_x'].isna()) & (df_annot['genPred_len_y'].notna())]
 df_annot = df_annot.rename(columns={"genPred_len_x":"genPred_len"})
 df_annot = df_annot.drop("genPred_len_y", axis=1)
